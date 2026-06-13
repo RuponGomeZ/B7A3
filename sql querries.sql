f11153c35004d33743e6594645ec0543e68bfc04
RuponGomeZ <anthonyrupon0@gmail.com>
2026-06-14 01:55:34 +0600

    user created

---
+++ sql querries.sql
@@ -0,0 +1,17 @@
+create database football_ticket_booking_system
+
+create table users(
+  user_id serial primary key,
+  full_name varchar(255),
+  email varchar(255) unique,
+  role varchar(50),
+  phone_number varchar(20)
+)
+
+INSERT INTO Users (user_id, full_name, email, role, phone_number) VALUES
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
+
+
